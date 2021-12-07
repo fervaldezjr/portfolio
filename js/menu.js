883faaca9d343d9d355f6aaa1bf29e1d419a7dee
Fernando Julián Valdez <82127006+fervaldezjr@users.noreply.github.com>
2021-12-07 22:57:22 +0100

    create a dark mode and finish all lenguages

--- js/menu.js
+++ js/menu.js
@@ -2,3 +2,10 @@
 ScrollReveal().reveal('.headline')
 ScrollReveal().reveal('.tagline', { delay: 450 })
 ScrollReveal().reveal('.punchline', { delay: 600 })
+
+// Dark Theme
+let icon = document.getElementById("icon");
+
+icon.onclick = function (){
+    document.body.classList.toggle("dark-theme");
+}
