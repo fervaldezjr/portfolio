34f1ce55f3a40f929ca03e2c54e0d2dc8ca0dc92
Fernando Valdez <fervaldezjr11@gmail.com>
2022-02-21 19:35:47 +0100

    changes addded

--- src/js/menu.js
+++ src/js/menu.js
@@ -8,13 +8,22 @@ function show() {
 };
 
 // Stack
-const stack = [
-    {nombre: 'JavaScript', logoTech: '../images/tecnologies/javascript-icon.png'},
-    {nombre: 'CSS', logoTech: '../images/tecnologies/css-icon.png'},
-    {nombre: 'SASS', logoTech: '../images/tecnologies/sass-icon.png'},
-    {nombre: 'Bootstrap', logoTech: '../images/tecnologies/bootstrap-icon.png'},
-    {nombre: 'HTML', logoTech: '../images/tecnologies/html-icon.png'},
-    {nombre: 'React', logoTech: '../images/tecnologies/react-icon.png'},
-    {nombre: 'Tailwind', logoTech: '../images/tecnologies/react-icon.png'},
-    {nombre: 'Node.js', logoTech: '../images/tecnologies/react-icon.png'}
+const stacks = [
+    {nombre: 'JavaScript', logoTech: '../images/tecnologies/javascript-icon.webp'},
+    {nombre: 'CSS', logoTech: '../images/tecnologies/css-icon.webp'},
+    {nombre: 'SASS', logoTech: '../images/tecnologies/sass-icon.webp'},
+    {nombre: 'Bootstrap', logoTech: '../images/tecnologies/bootstrap-icon.webp'},
+    {nombre: 'HTML', logoTech: '../images/tecnologies/html-icon.webp'},
+    {nombre: 'React', logoTech: '../images/tecnologies/react-icon.webp'},
+    {nombre: 'Tailwind', logoTech: '../images/tecnologies/tailwind-icon.webp'},
+    {nombre: 'Git', logoTech: '../images/tecnologies/git-icon.webp'},
+    {nombre: 'Node.js', logoTech: '../images/tecnologies/nodejs-icon.webp'}
 ];
+
+const stackCard = document.getElementById('stack');
+
+stackCard.innerHTML = stacks.map((stacks) => `
+    <div class='flex-col items-center justify-center text-center'>
+        <img src='${stacks.logoTech}' alt='${stacks.nombre}' class='flex justify-center items-center mx-auto' width='100px'/>
+    </div>
+`).join('');
